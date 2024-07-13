a66311e90cd2bdf9c9cd4b596944f631e83a76c8
RifatMahmudno-1 <rifatmahmudpc@gmail.com>
2024-07-13 16:46:20 +0600

    db updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
+let dbConnected = false
 
-const connectDB = () => {
-  mongoose
-    .connect(`${process.env.MongoUrl}/Chocolates-Data`)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.error('Could not connect to MongoDB', err));
-};
+const connectDB = () =>
+	mongoose
+		.connect(`${process.env.MongoUrl}/Chocolates-Data`)
+		.then(() => {
+			console.log('Connected to MongoDB')
+			dbConnected = true
+		})
+		.catch(err => {
+			dbConnected = false
+			console.error('Could not connect to MongoDB', err)
+		})
 
-export default connectDB;
+export { dbConnected, connectDB }
--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,19 @@
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import express from 'express'
-import connectDB from '../config/db.js'
+import { connectDB, dbConnected } from '../config/db.js'
 import chocolateRouter from './routes/chocolateRoute.js'
 import premiumRouter from './routes/premiumRoute.js'
 import vite from '../config/vite.js'
 
 const app = express()
-connectDB()
+
+// if (!dbConnected) await connectDB()
+app.use(async (req, res, next) => {
+	if (!dbConnected) await connectDB()
+	if (!dbConnected) return res.status(500).send({ error: true, success: false, message: "Couldn't connect to the database" })
+	else return next()
+})
 
 app.use(express.json())
 app.use(cors())
